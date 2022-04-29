20d54cfd51b880e2d355c743b2c8736dc32d55a6
myyong <myong@turing.ac.uk>
2022-04-29 14:42:43 +0100

    Log likelihood for alzheimers

--- examples/synthetic_data_example.py
+++ examples/synthetic_data_example.py
@@ -1,5 +1,5 @@
 from learning_machines_drift import datasets
-from learning_machines_drift import Registry, FileBackend
+from learning_machines_drift import Registry, Monitor, FileBackend, datasets
 import pandas as pd
 import numpy as np
 
@@ -12,25 +12,29 @@ features_df = reference_data[
     ["PLS-derived-gray-matter-scores", "beta-Amyloid-score", "APOE-4-status"]
 ]
 
-print(features_df)
 labels_df = reference_data[["Outcome"]]
 # # Log our reference dataset
-detector = Registry(tag="simple_example", backend=FileBackend("my-data"))
+detector = Registry(tag="alzheimer_example", backend=FileBackend("my-data"))
 detector.register_ref_dataset(features=features_df, labels=labels_df)
 
 monitor_1998_data = pd.read_csv(
     "examples/alzheimers-data/alzheimers_synthetic_1998.csv"
 )
 
-
 monitor_1998_features = monitor_1998_data[
     ["PLS-derived-gray-matter-scores", "beta-Amyloid-score", "APOE-4-status"]
 ]
-print(monitor_1998_features)
+
 # run gmlvq to get Y, Y_report
 monitor_1998_labels = reference_data[["Outcome"]]
 
 with detector:
     detector.log_features(monitor_1998_features)
     detector.log_labels(monitor_1998_labels)
-    print(detector.hypothesis_tests.kolmogorov_smirnov())
+
+measure = Monitor(tag="simple_example", backend=FileBackend("my-data"))
+measure.load_data()
+
+
+print("KS score: {}".format(measure.hypothesis_tests.kolmogorov_smirnov()))
+print("GM Likelihood score: {}".format(measure.hypothesis_tests.sdv_evaluate()))
