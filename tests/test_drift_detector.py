39835a372c2adc1de18021f50bc2efc518b7f7e8
Sam Greenbury <sgreenbury@turing.ac.uk>
2022-10-24 11:31:31 +0100

    Add test for loading with a drift_filter

--- tests/test_drift_detector.py
+++ tests/test_drift_detector.py
@@ -20,6 +20,7 @@ from learning_machines_drift import (  # DriftDetector,; DriftMeasure,
 from learning_machines_drift.backends import FileBackend
 from learning_machines_drift.datasets import example_dataset
 from learning_machines_drift.display import Display
+from learning_machines_drift.drift_filter import Filter
 
 N_FEATURES = 3
 N_LABELS = 2
@@ -433,6 +434,69 @@ def test_load_all_logged_data(tmp_path: pathlib.Path) -> None:
     dimensions = recovered_dataset.unify().shape
     assert dimensions[0] == 4
     assert dimensions[1] == 5
+
+
+def test_load_data_filtered(tmp_path: pathlib.Path) -> None:
+    """TODO PEP 257"""
+    det = Registry(tag="test", backend=FileBackend(tmp_path))
+    features_df, labels_df, latents_df = example_dataset(200)
+    det.register_ref_dataset(features=features_df, labels=labels_df, latents=latents_df)
+
+    # And we have features and predicted labels
+    features_reg, labels_reg, latents_reg = example_dataset(100)
+
+    # When we log features and labels of new data
+    with det:
+        det.log_features(features_reg)
+        det.log_labels(labels_reg)
+        det.log_latents(latents_reg)
+
+    # Make a drift filter
+    filter_dict = {
+        "age": ("less", 0.2),
+        "height": ("greater", -0.1),
+        "y": ("equal", 0),
+        "latents": ("greater", 0.6),
+    }
+    drift_filter = Filter(filter_dict)
+
+    # Load data (unfiltered datasets)
+    monitor_unfiltered = Monitor(tag="test", backend=FileBackend(tmp_path))
+    _ = monitor_unfiltered.load_data()
+
+    # Load data (filtered datasets)
+    monitor = Monitor(tag="test", backend=FileBackend(tmp_path))
+    _ = monitor.load_data(drift_filter)
+
+    # Assert conditions on unfiltered and filtered datasets
+    for (ref_dataset, reg_dataset, assertion_bool) in [
+        (monitor_unfiltered.ref_dataset, monitor_unfiltered.registered_dataset, False),
+        (monitor.ref_dataset, monitor.registered_dataset, True),
+    ]:
+        assert ref_dataset is not None and reg_dataset is not None
+        assert ref_dataset.latents is not None and reg_dataset.latents is not None
+        assert all(
+            [
+                ref_dataset.features.shape[0] != 0,
+                ref_dataset.labels.shape[0] == ref_dataset.latents.shape[0],
+                ref_dataset.labels.shape[0] == ref_dataset.latents.shape[0],
+            ]
+        )
+        assert all(
+            [
+                reg_dataset.features.shape[0] != 0,
+                reg_dataset.labels.shape[0] == reg_dataset.latents.shape[0],
+                reg_dataset.labels.shape[0] == reg_dataset.latents.shape[0],
+            ]
+        )
+        assert ref_dataset.features["age"].lt(0.2).all() == assertion_bool
+        assert reg_dataset.features["age"].lt(0.2).all() == assertion_bool
+        assert ref_dataset.features["height"].gt(-0.1).all() == assertion_bool
+        assert reg_dataset.features["height"].gt(-0.1).all() == assertion_bool
+        assert ref_dataset.labels.eq(0).all() == assertion_bool
+        assert reg_dataset.labels.eq(0).all() == assertion_bool
+        assert ref_dataset.latents["latents"].gt(0.6).all() == assertion_bool
+        assert reg_dataset.latents["latents"].gt(0.6).all() == assertion_bool
 
 
 def test_category_columns(tmp_path: pathlib.Path) -> None:
