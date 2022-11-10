46f1ccca2d0e099b881884d4601b46b2b18b1679
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2022-11-10 14:13:55 +0200

    preparation for sphinx documentation

---
+++ conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'learning-machines-drift'
+copyright = '2022, May Yong, Jannetta Steyn, Sam Greenbury'
+author = 'May Yong, Jannetta Steyn, Sam Greenbury'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'bizstyle'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,25 @@
+.. learning-machines-drift documentation master file, created by
+   sphinx-quickstart on Tue Nov  1 17:18:07 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to learning-machines-drift's documentation!
+===================================================
+
+A python package for monitoring dataset drift in production ML pipelines.
+
+Built to run in any environment without uploading your data to external services.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   README
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
