4dae358a548452f1381416d0b96408c03071c0b4
Sam Greenbury <sgreenbury@turing.ac.uk>
2023-01-19 17:21:42 +0000

    Add extension for google style docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,6 +20,7 @@ extensions = [
     "sphinx.ext.duration",
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
 ]
 
 templates_path = ["_templates"]
@@ -30,4 +31,4 @@ templates_path = ["_templates"]
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
