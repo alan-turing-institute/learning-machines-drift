a3dc5c063e8d5dd296e4b4bc857fc4d28d970861
Jannetta Steyn <6432530+jsteyn@users.noreply.github.com>
2023-03-01 10:25:54 +0000

    Add paths to config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,13 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+sys.path.insert(0, os.path.abspath("../.."))
+
 project = "learning-machines-drift"
 copyright = "2023, Alan Turing Institute"
 author = (
